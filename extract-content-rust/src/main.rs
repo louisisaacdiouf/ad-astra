394c520653c8eca5ff384da4e11d86e02859c6fb
lodiouf <louisisaacdf@gmail.com>
2025-02-08 00:51:32 +0000

    Extraction de livre

--- extract-content-rust/src/main.rs
+++ extract-content-rust/src/main.rs
@@ -2,7 +2,7 @@ use std::time::Instant;
 
 fn main() {
     let start = Instant::now();
-    let bytes = std::fs::read("fichier1.pdf").unwrap();
+    let bytes = std::fs::read("livre.pdf").unwrap();
     let out = pdf_extract::extract_text_from_mem(&bytes).unwrap();
     println!("{out}");
     println!("\n\nDuration: {:?}", start.elapsed());
