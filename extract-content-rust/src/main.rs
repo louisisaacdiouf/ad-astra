e23929f222d32b2e6024bc7f4268098036a01552
lodiouf <louisisaacdf@gmail.com>
2025-02-08 00:25:32 +0000

    Extraction de données Rust

---
+++ extract-content-rust/src/main.rs
@@ -0,0 +1,9 @@
+use std::time::Instant;
+
+fn main() {
+    let start = Instant::now();
+    let bytes = std::fs::read("fichier1.pdf").unwrap();
+    let out = pdf_extract::extract_text_from_mem(&bytes).unwrap();
+    println!("{out}");
+    println!("\n\nDuration: {:?}", start.elapsed());
+}
